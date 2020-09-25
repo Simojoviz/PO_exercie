e5fad56cad728113f668e0510fe4a6e8c75b509b
Simojoviz <simonejov@gmail.com>
2020-09-25 21:51:48 +0200

    LinkedList main that puts vehicle in the list nodes

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,5 +1,19 @@
 public class LinkedList {
     Node head;
     Node last;
+
+    public static void main(String[] args) {
+        LinkedList vehicleList = new LinkedList();
+
+        for (int i = 0; i < 4; i++) {
+            if (vehicleList.last == null) {
+                vehicleList.head = new Node(new Vehicle());
+                vehicleList.last = vehicleList.head;
+            } else {
+                Node newNode = new Node(new Vehicle(), vehicleList.head);
+                vehicleList.head = newNode;
+            }
+        }
+    }
 }
 
--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,13 @@
 public class Node {
     Object elem;
     Node nextNode;
+
+    public Node(Object elem, Node nextNode) {
+        this.elem = elem;
+        this.nextNode = nextNode;
+    }
+
+    public Node(Object elem) {
+        this(elem, null);
+    }
 }
