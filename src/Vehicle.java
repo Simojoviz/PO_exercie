583f2cb694f99ef3bf1a40d0685e7781939e3b22
Simojoviz <simonejov@gmail.com>
2020-09-25 12:14:15 +0200

    adding main

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,5 +2,9 @@ public class Vehicle {
     int speed;
     int direction;
     String ownerName;
-    
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle();
+        System.out.println("Hello");
+    }
 }
